9b25bd84e44b1b68cc375cf2dd686b6a374aad0f
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-25 15:00:10 -0500

    vista principal

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = ({comments}) => {
+  return(
+    <div className='text-center container-fluid bg-blue big-font'>
+      <div className='container'>
+        <div className="bg-light-blue">
+          <form>
+          <h3> NEW COMMENT </h3>
+            <input type="text" name="name" placeholder="Add name"/><br/>
+            <input type="text" name="comment" placeholder="Add comments"/><br/>
+            <button type="submit" name="submit" value="submit">POST COMMENT</button>
+          </form>
+        </div>
+        <div>
+          <h4>COMMENTS</h4>
+          <p className="contador">{comments.length} Comments:</p>
+          <ul></ul>
+        </div>
       </div>
+    </div>
     );
-  }
 }
 
 export default App;
