48f0d59f71acc2653e256bf55eb84c13a02d10bd
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-26 05:12:23 -0500

    agregando el avaatar al objeto

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import avatar1 from './icons/alien-1.png';
+import avatar2 from './icons/alien.png';
+
 let comments = [
     {
+        avatar: avatar1,
         name: 'Morgan McCircuit',
         comment: 'Content marked as abusive'
     },
     {
+        avatar: avatar2,
         name: 'Bending Bender',
         comment: 'Excellent stuff'
     }
